958abcb7fef053caef7ad5ef747959c451c2f98e
Victoria Sanchez Reyes <vsanchez636@gmail.com>
2012-10-19 00:25:29 -0300

    Se redefine la accion de busqueda y guardar
    Los cambios realizados logran almacenar los resultados de la busqueda
    en la base de datos.

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -6,44 +6,26 @@ class TwitterController < ApplicationController
 		# Formulario
 	end
 
-	def busqueda
-		session[:cliente] = TwitterSearch::Client.new 'Buscar tweets'
-		busqueda = Palabra.new params[:busqueda]
-		busqueda.save
-		session[:palabra] = busqueda.word
-		redirect_to tweets_path
-
-	rescue Exception => e
-		puts e.message
-		puts busqueda.inspect
-		redirect_to index_path
-
-	end
-
 	def tweets
 		# Muestra los tweets
-		@palabra = session[:palabra]
-		@tweets = session[:cliente].query :q => @palabra, :result_type => "recent", :rpp => 50
-		id = params[:id].to_i
-		@word = Palabra.get id
+		cliente = TwitterSearch::Client.new 'Buscar tweets'
+		@palabra = params[:busqueda][:word]
+		@tweets = cliente.query :q => @palabra, :result_type => "recent", :rpp => '10'
 	end
 
 	def guardar
-		id = params[:id].to_i
-		word = Palabra.get id
-		palabra = session[:palabra]
-		tweets = session[:cliente].query :q => palabra, :result_type => "recent", :rpp => 50
-		guardar = Tweet.new
-		guardar.attributes = { :user_name => tweets.from_user, :text => tweets.text, :image => tweets.profile_image_url, :date => tweets.created_at, :palabra_id => word}
-		guardar.save
-		redirect_to tweets_path
-
-	rescue Exception => e
-		puts e.message
-		puts guardar.inspect
+		# Guarda todos los datos en la base de datos
+		Palabra.transaction do	
+			word = Palabra.create params[:busqueda]
+			params[:tweets].each do |tweet|
+	    		word.tweets.create tweet
+			end
+		end	
 		redirect_to index_path
+
+	rescue DataMapper::SaveFailureError => e
+		puts e.message
+		puts e.resource.errors.inspect
 	end
-
-
 
 end
